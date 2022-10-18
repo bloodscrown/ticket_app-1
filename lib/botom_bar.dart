95e8b8a2223d43ee77adda6a19a4b4e61b5ef679
Areeba Jabeen <areeba.jabeen21@gmail.com>
2022-10-18 23:43:40 +0500

    homescreen started

--- lib/botom_bar.dart
+++ lib/botom_bar.dart
@@ -11,7 +11,7 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
-  int _selectedItem = 0;
+  int _selectedIndex = 0;
   static final List<Widget> _widgetOptions = <Widget>[
     const Text("home"),
     const Text("Search"),
@@ -20,21 +20,18 @@ class _BottomBarState extends State<BottomBar> {
   ];
   void itemOnTapped(int index) {
     setState(() {
-      _selectedItem = index;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("first app"),
-      ),
       body: Center(
-        child: _widgetOptions[0],
+        child: _widgetOptions[_selectedIndex],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedItem,
+          currentIndex: _selectedIndex,
           onTap: itemOnTapped,
           elevation: 10,
           showSelectedLabels: false,
@@ -51,7 +48,7 @@ class _BottomBarState extends State<BottomBar> {
                 activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
                 label: "search"),
             BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+                icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
                 activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
                 label: "ticket"),
             BottomNavigationBarItem(
