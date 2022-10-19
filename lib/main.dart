32b083a4896a521d9c6d8c5ac2c1dec35fc87606
Areeba Jabeen <areeba.jabeen21@gmail.com>
2022-10-19 19:14:05 +0500

    airplane design compeleted here

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "2nd try",
       theme: ThemeData(primaryColor: primary),
       home: const BottomBar(),
--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,5 +1,6 @@
 import 'package:first_app/uitles/app_layout.dart';
 import 'package:first_app/uitles/app_style.dart';
+import 'package:first_app/uitles/tick_contanier.dart';
 import 'package:flutter/animation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -35,14 +36,62 @@ class TicketView extends StatelessWidget {
                         style:
                             Styles.headLinestyle3.copyWith(color: Colors.white),
                       ),
-                      const Spacer(),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      const ThickContanier(),
+                      Expanded(
+                        child: Stack(
+                          children: [
+                            SizedBox(
+                              height: 24,
+                              child: LayoutBuilder(
+                                builder: (BuildContext context,
+                                    BoxConstraints constraints) {
+                                  return Flex(
+                                    direction: Axis.horizontal,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: List.generate(
+                                        (constraints.constrainWidth() / 6)
+                                            .floor(),
+                                        (index) => const SizedBox(
+                                              width: 3,
+                                              height: 1,
+                                              child: DecoratedBox(
+                                                decoration: BoxDecoration(
+                                                    color: Colors.white),
+                                              ),
+                                            )),
+                                  );
+                                },
+                              ),
+                            ),
+                            Center(
+                              child: Transform.rotate(
+                                angle: 1.5,
+                                child: const Icon(
+                                  Icons.local_airport_rounded,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const ThickContanier(),
+                      Expanded(
+                        child: Container(),
+                      ),
                       Text(
                         "ISL",
                         style:
                             Styles.headLinestyle3.copyWith(color: Colors.white),
                       )
                     ],
-                  )
+                  ),
+                //airplane design compeleted here
                 ],
               ),
             )
